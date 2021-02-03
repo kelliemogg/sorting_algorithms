b7605b3d968fb604f47ac41b48dfc359d274fe70
Ben Dosch <Benjamindosch@gmail.com>
2021-02-03 20:33:38 +0000

    time complexity and header

--- sort.h
+++ sort.h
@@ -29,5 +29,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 listint_t *swap_nodes(listint_t **list, listint_t *tmp);
 void cocktail_sort_list(listint_t **list);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif /* SORT_H */
